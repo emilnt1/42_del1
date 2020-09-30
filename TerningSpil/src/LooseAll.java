50827be7e3e59de73061053f82d68e63577c9892
Emil Trangeled <emilbfh@hotmail.com>
2020-09-30 18:35:48 +0200

    "Public static LooseAll" added

--- TerningSpil/src/LooseAll.java
+++ TerningSpil/src/LooseAll.java
@@ -3,19 +3,22 @@
 //Spilleren mister alle sine point hvis spilleren slår to 1'ere
 public class LooseAll {
 
-    private boolean P1,P2;
-    private int d1, d2;
-    private int pointP1, pointP2;
+    public static LooseAll (Player current, Dice d1, Dice d2){
 
-    //Hvis det er P1's tur, og deres sum af kast er 2, bliver deres point sat til 0
-    if ((P1 == true) && (d1 + d2 == 2)){
-        pointP1 = 0;
+        private boolean P1,P2;
+        private int d1, d2;
+        private int pointP1, pointP2;
+
+        //Hvis det er P1's tur, og deres sum af kast er 2, bliver deres point sat til 0
+        if ((P1 == true) && (d1 + d2 == 2)){
+            pointP1 = 0;
+        }
+
+
+        //Hvis det er P2's tur, og deres sum af kast er 2, bliver deres point sat til 0
+        if ((P2 == true) && (d1 + d2 == 2)){
+            pointP2 = 0;
+        }
+
     }
-
-
-    //Hvis det er P2's tur, og deres sum af kast er 2, bliver deres point sat til 0
-    if ((P2 == true) && (d1 + d2 == 2)){
-        pointP2 = 0;
-    }
-
 }
