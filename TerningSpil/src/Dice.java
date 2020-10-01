476237f34d132465922831f9fc12fb733f0633c9
Chris2670 <70951742+Chris2670@users.noreply.github.com>
2020-10-01 10:32:01 +0200

    Dice feature version 1

--- TerningSpil/src/Dice.java
+++ TerningSpil/src/Dice.java
@@ -1,8 +1,31 @@
-
 import java.lang.Math;
 
-public class Dice {
-    public static void main(String[] args) {
-        System.out.println("test");
+public class Dice {  //her defineres klassen "Dice"
+    private final int MAX =6; //værdi af terning kan max være 6
+    private int facevalue; //facevalue er den int, som beskriver hvad terningen slår
+
+    public Dice(int value){ //giver dicen en int value
+        facevalue = value; //
+    }
+
+
+    public int getFacevalue() { //her henter systemet facevaluen
+        return facevalue;
+    }
+
+
+    public int rollDice(){ // den int value man får efter man har slået med terningen
+        facevalue = (int)(Math.random()*6)+1;
+        return facevalue;
+    }
+
+    public String toString(){  //en string der siger hvad man har slået
+        String ln = "value" + getFacevalue();
+        return ln;
+    }
+
+    public void setFacevalue(int value) {
+        facevalue = value;
     }
 }
+
