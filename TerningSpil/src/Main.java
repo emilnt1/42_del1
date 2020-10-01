af81d0c9ed18490ed32bf1c10ea439275f46407e
Emil Trangeled <emilbfh@hotmail.com>
2020-10-01 13:21:25 +0200

    LooseAllClass and extraTurnClass, is merged into Rules. And Main has been changed accordingly

--- TerningSpil/src/Main.java
+++ TerningSpil/src/Main.java
@@ -1,15 +1,13 @@
-import java.awt.*;
+
 import java.util.Scanner;
 
-import gui_fields.GUI_Field;
-import gui_main.GUI;
 public class Main {
 
     public static void main(String[] args) {
         Player P1 = new Player(0);
         Player P2 = new Player(0);
         Player current;
-        Current player = new Current(1);
+        Rules player = new Rules(1);
         Dice D1 = new Dice(0);
         Dice D2 = new Dice(0);
         Dice D3 = new Dice(0);
@@ -20,15 +18,15 @@ public class Main {
             input.next();
             D1.rollDice();
             D2.rollDice();
-            current = Current.shiftPlayer(P1,P2,player);
-            Current.addToScore(D1,D2,current);
-            Current.loseAll(D1,D2,current);
-            Current.extraTurn(D1,D2,player);
-            Current.win(D1,D2,D3,D4,current);
+            current = Rules.shiftPlayer(P1,P2,player);
+            Rules.addToScore(D1,D2,current);
+            Rules.loseAll(D1,D2,current);
+            Rules.extraTurn(D1,D2,player);
+            Rules.win(D1,D2,D3,D4,current);
             Display.Display(D1,D2,P1,P2);
-            System.out.println("Player " + player.getPlayer()+"'s turn");
+            System.out.println("Player " + player.getPlayerID()+"'s turn");
             if(current.getScore() >= 40){
-                if(player.getPlayer()==1){
+                if(player.getPlayerID()==1){
                     System.out.println("Player 2 wins");
                 } else {
                     System.out.println("Player 1 wins");
--- TerningSpil/src/Rules.java
+++ TerningSpil/src/Rules.java
@@ -33,6 +33,33 @@ public class Rules {
         Rules.addScore( a.getFacevalue() + b.getFacevalue());
     }
 
+    // Score is set to 0 if player gets a sum of 2
+    public static void loseAll(Dice d1, Dice d2, Player current){
+
+        //Hvis det er P1's tur, og deres sum af kast er 2, bliver deres point sat til 0
+        if (d1.getFacevalue() + d2.getFacevalue() == 2){
+            current.setScore(0);
+        }
+
+
+    }
+
+    public static void extraTurn (Dice d1, Dice d2, Rules ID){
+
+        if (d1 == d2){
+            if (ID.getPlayerID() == 1){
+                ID.setPlayerID(2);
+
+
+            } else {
+                ID.setPlayerID(1);
+
+            }
+
+        }
+
+    }
+
 
 }
 
