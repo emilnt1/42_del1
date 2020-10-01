4032e9a92590afe0415021afc75a8c146b309da5
Spastinak <71653506+Spastinak@users.noreply.github.com>
2020-10-01 09:57:31 +0200

    opdateret kommentar

--- TerningSpil/src/Rules.java
+++ TerningSpil/src/Rules.java
@@ -16,18 +16,18 @@ public class Rules {
 
 // player switch: switch who is currently playing
 
-    public static Player shiftPlayer( Player one, Player two, Rules a){
-        if (a.getPlayerID() == 1){
-            a.setPlayerID(2);
+    public static Player shiftPlayer( Player one, Player two, Rules ID){
+        if (ID.getPlayerID() == 1){
+            ID.setPlayerID(2);
             return one;
 
         } else {
-            a.setPlayerID(1);
+            ID.setPlayerID(1);
             return two;
         }
     }
 
-// add value of current score to each player
+// add score value to the currently playing player
 
     public static void addToScore (Dice a, Dice b, Player Rules){
         Rules.addScore( a.getFacevalue() + b.getFacevalue());
