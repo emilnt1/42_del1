f1afd65038d03ed0504c3017ff65da4408529378
Machnuud <Albert88->
2020-10-02 11:56:22 +0200

    Roll is now a button

--- TerningSpil/src/Main.java
+++ TerningSpil/src/Main.java
@@ -20,53 +20,52 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.println("To start game press anything and press enter, to exit write \"exit\" and press enter.");
-        System.out.println("To roll again press anything and enter again.");
-        while(input.hasNextLine()) {
+        System.out.println("To roll press the roll button on the interface.");
 
+        while(input.hasNext()) {
+            word = input.nextLine().toLowerCase();
+            if(word.equals("exit")) {
+                System.exit(0);
+            }
             D1.setFacevalue(0);
             D2.setFacevalue(0);
             P1.reset();
             P2.reset();
             player.setPlayerID(1);
-            while (input.hasNextLine()) {
-                word = input.nextLine();
-                word = word.toLowerCase();
-                if(word.equals("exit")){
-                    break;
-                }
-                gui.setDice(D1.rollDice(), D2.rollDice());
-                current = Rules.shiftPlayer(P1, P2, player);
+                while(gui.getUserLeftButtonPressed("Press left button to roll","roll","-")){
+                    gui.setDice(D1.rollDice(), D2.rollDice());
+                    current = Rules.shiftPlayer(P1, P2, player);
 
-                if (current.getSuddenD() == 1) {
-                    Display.display(D1, D2, P1, P2);
-                    if (D1.getFacevalue() == D2.getFacevalue()) {
-                        break;
-                    } else {
-                        System.out.println("Player " + player.getPlayerID() + "'s turn");
+                    if (current.getSuddenD() == 1) {
+                        Display.display(D1, D2, P1, P2);
+                        if (D1.getFacevalue() == D2.getFacevalue()) {
+                            break;
+                        } else {
+                            System.out.println();
+                            System.out.println("Player " + player.getPlayerID() + "'s turn");
+                            System.out.println();
+                        }
                     }
-                }
 
-                if (current.getSuddenD() == 0) {
-                    Rules.addToScore(D1, D2, current);
-                    Rules.loseAll(D1, D2, current);
-                    Rules.extraTurn(D1, D2, player);
+                    if (current.getSuddenD() == 0) {
+                        Rules.addToScore(D1, D2, current);
+                        Rules.loseAll(D1, D2, current);
+                        Rules.extraTurn(D1, D2, player);
                     if(Rules.win(current, D1, D2)){
                         break;
                     }
                     Display.display(D1, D2, P1, P2);
+                    System.out.println();
                     System.out.println("Player " + player.getPlayerID() + "'s turn");
+                    System.out.println();
                     Rules.suddenD(current);
                 }
 
             }
-            if(word.equals("exit")) {
-                System.exit(0);
-            }
+
+
             System.out.println("Player " + player.getPlayerID() + " wins");
-            System.out.println("To restart game press anything and press enter, to \"exit\"  write exit and press enter.");
+            System.out.println("To restart game press anything and press enter, to exit write \"exit\" and press enter.");
         }
-        input.close();
     }
-
-
 }
