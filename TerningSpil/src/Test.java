9eda22fd88d69f08cb18da79f2fefafed0ba7de3
Zahed <zahedm45@gmail.com>
2020-10-02 15:40:10 +0200

    Test for test class.

--- TerningSpil/src/Test.java
+++ TerningSpil/src/Test.java
@@ -1,33 +1,17 @@
-import gui_main.GUI;
+
+import java.math.*;
 
 public class Test {
     public static void main(String[] args) {
-//
-//        GUI gui = new GUI();
-//        // Loop for player-1
-//        for(int a = 0; a < 1000; a++){
-//
-//            Player P1 = new Player(0,0,0,0);
-//            Player P2 = new Player(0,0,0,0);
-//            Player current;
-//            Rules player = new Rules(1);
-//            Dice D1 = new Dice(0);
-//            Dice D2 = new Dice(0);
-//            gui.setDice(D1.rollDice(), D2.rollDice());
-//            System.out.println("Player-1 " +D1);
-//        }
-//        //Loop for player-2
-//        for(int b = 0; b < 1000; b++){
-//            Player P1 = new Player(0,0,0,0);
-//            Player P2 = new Player(0,0,0,0);
-//            Player current;
-//            Rules player = new Rules(1);
-//            Dice D1 = new Dice(0);
-//            Dice D2 = new Dice(0);
-//            gui.setDice(D1.rollDice(), D2.rollDice());
-//            System.out.println("Player-2 " +D2);
-//        }
 
+        for(int a = 0; a < 1000; a++) {
+        Dice p1 = new Dice(0);
+        p1.rollDice();
+        Dice p2= new Dice(0);
+        p2.rollDice();
+        System.out.println(p2+" "+p2);
+
+        }
    }
 
 }
